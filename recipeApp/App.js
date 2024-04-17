82ee5d26a6be8924500b55db348d5fd42068023a
Niki9526 <n01591561@humber.ca>
2024-04-16 23:23:18 -0400

    added nav to login, del space

--- recipeApp/App.js
+++ recipeApp/App.js
@@ -6,22 +6,20 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Home from './screens/Home';
+import Login from './screens/Login';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   
   return (
+
   <NavigationContainer>
   <Stack.Navigator>
   <Stack.Screen name="Home" component={Home} />
- 
-
+  <Stack.Screen name="Login" component={Login} />
   </Stack.Navigator>
   </NavigationContainer>
-
-   
-   
   );
 }
 
@@ -36,5 +34,4 @@ const styles = StyleSheet.create({
     flex:1,
     backgroundColor: "cornsilk",    
 },
-
 });
