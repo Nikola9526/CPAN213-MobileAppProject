22b4d18169da1705737366cecf17a2e204637747
Niki9526 <n01591561@humber.ca>
2024-04-18 16:43:16 -0400

    added posting of recipes to api

--- recipeApp/screens/addRecipe.js
+++ recipeApp/screens/addRecipe.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 
-import { FlatList, StyleSheet, Text, View, Alert } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Alert, TextInput , Button} from 'react-native';
 import { AuthContext } from '../store/auth-context';
 
 
@@ -9,28 +9,103 @@ import { AuthContext } from '../store/auth-context';
 
 
 function AddRecipe() {
-    
+  const [postData, setPostData] = useState({
+    name: '',
+    description: ''
+  })
   
+    
     const authCtx = useContext(AuthContext);
     const token = authCtx.token;
+    const username= authCtx.user;
   
-    useEffect(() => {
+    /*useEffect(() => {
       axios
-        .get(
-          'http://10.0.2.2:9001/api/posts/all'
+        .post(
+          'http://10.0.2.2:9001/api/posts/add' + token
         )
         .then((response) => {
           setRecipes(response.data);
           //Alert.alert('You authenticated successfully!')
     
         });
-    }, []);//[token
+    }, []);*///[token
+
+    const handlepostData = async () => {
+      
+      try{
+        const response = await axios.post(
+          "http://10.0.2.2:9001/api/posts/add" +
+          postData,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: token,
+            },
+          }
+        );
+        // Handle success response
+        console.log("Response:", response.data);
+        Alert.alert("Success", "Data posted successfully");
+
+        // Clear the form after successful submission
+        setPostData({
+          title: "",
+          description: "",
+          // Reset other fields as needed
+        });
+
+      } catch (error) {
+        console.error('Error posting data:', error)
+        Alert.alert('Error', 'Failed to Add Recipe!' )
+      }
+    };
     
     return (
       <View style={styles.rootContainer}>
          
-       <Text style={styles.title}>nikola95 </Text>
+       <Text style={styles.title}>{username}</Text>
        <Text style={styles.subTitle}>Add a Recipe</Text>
+       <View style={{ backgroundColor: 'white', padding: 15, borderRadius:10}}>
+       
+       <TextInput
+       
+       placeholder='Enter Name'
+       value={postData.name} 
+        onChangeText={(text) => setPostData({...postData, name: text})}
+       />
+        <TextInput
+       placeholder='Enter Description'
+       value={postData.description} 
+        onChangeText={(text)=> setPostData({...postData, description: text})}
+       />
+       {/*<TextInput
+       placeholder='Enter Info Comma Separted Values '
+       value={info} 
+        onChangeText={setInfo}
+       />
+       <TextInput
+       placeholder='Enter Serving Size'
+       value={servingsize} 
+        onChangeText={setServingsize}
+       />
+        <TextInput
+       placeholder='Enter Ingredients: comma sep values'
+       value={ingredients} 
+        onChangeText={setIngredients}
+       />
+       <TextInput
+       placeholder='Enter Directions: comma sep values'
+       value={directions} 
+        onChangeText={setDirections}
+       />
+       <TextInput
+       placeholder='Enter Note'
+       value={note} 
+        onChangeText={setNote}
+       />*/}
+       </View>
+       <Button title="Add" onPress={handlepostData} />
       
         
         <Text></Text>
@@ -56,7 +131,8 @@ function AddRecipe() {
     subTitle: {
       fontSize: 16,
       fontWeight: "bold",
-      marginBottom: 8,
+      marginBottom: 50,
+
   
     },
     
@@ -73,4 +149,4 @@ function AddRecipe() {
     },
     itemText: { color: "white" },
   });
-  
+  //<Text style={{ textAlign: 'left'}}>Name:</Text>
